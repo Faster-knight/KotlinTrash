c02fd39c1638adc02d38d8873a57a8b1b0545311
Faster-knight <postnikovtrofim1@gmail.com>
2024-12-05 15:04:52 +0300

    Add re xss defend expressions base

--- mobileClient/src/CoreApplication.ts
+++ mobileClient/src/CoreApplication.ts
@@ -3,6 +3,11 @@ import { createContext } from "react";
 
 type LanguageApplicationSupport = "ru" | "eng" | "chi"
 
+export function XSS_injectionJavaScript(input: string): boolean {return /alert|ALERT|document|DOCUMENT|fetch|FETCH|<|>|script|SCRIPT/.test(input.toLowerCase())}
+export function XSS_injectionSQL(input: string): boolean {return /SQL|SELECT|select|sql|from|FROM|drop|DROP|put|PUT|insert|INSERT/.test(input.toLowerCase())}
+export function XSS_injectionSVG(input: string): boolean {return /SVG|<|>|svg/.test(input.toLowerCase())}
+export function XSS_injectionHTML(input: string): boolean {return /<|>|html|HTML|alert/.test(input.toLowerCase())}
+export function Check_domainMailInput(input: string): boolean {return /^[a-z]+@[a-z]+$/.test(input.toLowerCase())}
 export interface ApplicationContextType {
     themeMode: number,
     setThemeMode: (newNumberMode: number) => void,
